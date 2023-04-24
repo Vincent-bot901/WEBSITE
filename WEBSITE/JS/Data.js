469a839ab273d8c226df94ffe8297d9857706876
Vincent-bot901 <61154320+Vincent-bot901@users.noreply.github.com>
2023-04-24 14:29:03 +0200

    Update Data.js

--- WEBSITE/JS/Data.js
+++ WEBSITE/JS/Data.js
@@ -4,42 +4,39 @@
   let FN;
   let listeMedValgte = [];
   let data;
-  var overskrift = null
-  var beskrivelse = null
+  var Options = null
 
 function ny() {
   var pic;
   x = Math.floor(Math.random()*4+1);
   genererTekst(x);
   pic = "https://github.com/mpsteenstrup/GCP4/blob/master/FN_maal/FN"+x+".jpg?raw=true"  
-  document.getElementById('billedtekst').innerHTML = overskrift;
-  document.getElementById('beskrivelse').innerHTML = beskrivelse;
+  document.getElementById('Options').innerHTML = Options;
   document.getElementById('myImage').src = pic;
 }
 
 function myFunction() {
   document.getElementById("frm1").submit();
 }
- 
+
 function valgte(){
   console.log(x);
   console.log(FN);
-  listeMedValgte.push(overskrift)
+  listeMedValgte.push(Options)
   document.getElementById('valg').innerHTML = listeMedValgte;
 }
 
 function genererTekst(x){
   for (var i = 0; i < data.length; i++) {
       if (data[i]["ID"] == x) {
-        overskrift = data[i]['overskrift']; 
-        beskrivelse = data[i]['beskrivelse']
+        Options = data[i]['Options']; 
         break; // exit efter loop
       }
     }
 }
 window.onload = function() {
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "FN.csv", true);
+xhr.open("GET", "WEBSITE/SHEETUPTHESCHOOL - Ark1.csv", true);
 xhr.responseType = "text";
 xhr.onload = function() {
   data = Papa.parse(xhr.responseText, {
