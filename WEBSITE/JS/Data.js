2759cc2f601ffa3040b7da2c0af3a634fdb884ad
Vincent-bot901 <61154320+Vincent-bot901@users.noreply.github.com>
2023-04-24 15:15:32 +0200

    Update Data.js

--- WEBSITE/JS/Data.js
+++ WEBSITE/JS/Data.js
@@ -4,14 +4,14 @@
   let FN;
   let listeMedValgte = [];
   let data;
-  var Options = null
+  var ID = null
   var Song = null
   
 
 function ny() {
   x = Math.floor(Math.random()*4+1);
   genererTekst(x);
-  document.getElementById('Options').innerHTML = Options;
+  document.getElementById('ID').innerHTML = ID;
   document.getElementById('Youtube').src = Song;
 }
 
@@ -22,14 +22,14 @@ function myFunction() {
 function valgte(){
   console.log(x);
   console.log(FN);
-  listeMedValgte.push(Options)
+  listeMedValgte.push(ID)
   document.getElementById('valg').innerHTML = listeMedValgte;
 }
 
 function genererTekst(x){
   for (var i = 0; i < data.length; i++) {
       if (data[i]["ID"] == x) {
-        Options = data[i]['Options'];
+        ID = data[i]['ID'];
         Song = data[i]['Link'];
         break; // exit efter loop
       }
